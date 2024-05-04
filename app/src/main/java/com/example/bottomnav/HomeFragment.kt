26be43f464f3ea7d061af5328c97e7f914fc0d90
Chua Jun Yu <chuajunyu911@gmail.com>
2024-05-04 16:34:48 +0800

    s

--- app/src/main/java/com/example/bottomnav/HomeFragment.kt
+++ app/src/main/java/com/example/bottomnav/HomeFragment.kt
@@ -1,18 +1,136 @@
 package com.example.bottomnav
 
+import android.annotation.SuppressLint
+import android.app.usage.UsageStats
+import android.app.usage.UsageStatsManager
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.provider.Settings
+import android.view.*
+import android.widget.TextView
+import androidx.appcompat.widget.Toolbar
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.bottomnav.databinding.FragmentHomeBinding
+import androidx.core.content.ContextCompat
+
 
 class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+    private lateinit var usageStatsManager: UsageStatsManager
+    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    private lateinit var viewManager: RecyclerView.LayoutManager
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true) // This line is important to show the menu in a Fragment
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val toolbar: Toolbar = view.findViewById(R.id.toolbar)
+        toolbar.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.purple_700))
+        (activity as AppCompatActivity).setSupportActionBar(toolbar)
+        (activity as AppCompatActivity).supportActionBar?.title = "Screen Time"
+
+        usageStatsManager = context?.getSystemService(Context.USAGE_STATS_SERVICE) as UsageStatsManager
+
+        viewManager = LinearLayoutManager(context)
+        viewAdapter = MyAdapter(emptyList())
+
+        binding.recyclerView.apply {
+            setHasFixedSize(true)
+            layoutManager = viewManager
+            adapter = viewAdapter
+        }
+
+        binding.getStatsButton.setOnClickListener {
+            val usageStats = getUsageStats()
+            displayUsageStats(usageStats)
+        }
+
+        binding.openSettingsButton.setOnClickListener {
+            startActivity(Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS))
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_main, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_reload -> {
+                val usageStats = getUsageStats()
+                displayUsageStats(usageStats)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun getUsageStats(): List<UsageStats> {
+        val endTime = System.currentTimeMillis()
+        val beginTime = endTime - 1000 * 60 * 60 * 24 // 1 day
+
+        val usageStatsList = usageStatsManager.queryUsageStats(UsageStatsManager.INTERVAL_DAILY, beginTime, endTime)
+        return usageStatsList.filter { it.totalTimeInForeground > 0 }
+    }
+
+    private fun displayUsageStats(usageStats: List<UsageStats>) {
+        viewAdapter = MyAdapter(usageStats)
+        binding.recyclerView.adapter = viewAdapter
+    }
+
+    inner class MyAdapter(private val myDataset: List<UsageStats>) :
+        RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
+
+        inner class MyViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
+
+        override fun onCreateViewHolder(parent: ViewGroup,
+                                        viewType: Int): MyAdapter.MyViewHolder {
+            val textView = TextView(parent.context)
+            return MyViewHolder(textView)
+        }
+
+        @SuppressLint("SetTextI18n")
+        override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+            val pm: PackageManager = context?.packageManager!!
+            try {
+                val applicationInfo = pm.getApplicationInfo(myDataset[position].packageName, 0)
+                val appName = pm.getApplicationLabel(applicationInfo).toString()
+
+                val totalSeconds = myDataset[position].totalTimeInForeground / 1000
+                val hours = totalSeconds / 3600
+                val minutes = (totalSeconds % 3600) / 60
+                val seconds = totalSeconds % 60
+
+                holder.textView.text = "$appName: ${String.format("%d:%02d:%02d", hours, minutes, seconds)}"
+            } catch (e: PackageManager.NameNotFoundException) {
+                val totalSeconds = myDataset[position].totalTimeInForeground / 1000
+                val hours = totalSeconds / 3600
+                val minutes = (totalSeconds % 3600) / 60
+                val seconds = totalSeconds % 60
+
+                holder.textView.text = "${myDataset[position].packageName}: ${String.format("%d:%02d:%02d", hours, minutes, seconds)}"
+            }
+        }
+
+        override fun getItemCount() = myDataset.size
     }
 }
--- app/src/main/java/com/example/bottomnav/MainActivity2.kt
+++ app/src/main/java/com/example/bottomnav/MainActivity2.kt
@@ -1,11 +1,13 @@
 package com.example.bottomnav
 
+import android.app.AlertDialog
+import android.app.AppOpsManager
+import android.content.Context
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
+import android.content.Intent
+import android.provider.Settings
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity2 : AppCompatActivity() {
@@ -16,12 +18,30 @@ class MainActivity2 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        if (!hasUsageStatsPermission()) {
+
+            AlertDialog.Builder(this)
+                .setTitle("Permission Required")
+                .setMessage("This app requires usage access settings to function. Please grant the permission.")
+                .setPositiveButton("Open Settings") { _, _ ->
+                    startActivity(Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS))
+                }
+                .setNegativeButton("Cancel", null)
+                .show()
+        }
+
+
         bottomNavigationView = findViewById(R.id.bottom_navigation)
 
         bottomNavigationView.setOnItemSelectedListener { menuItem ->
             when(menuItem.itemId){
                 R.id.bottom_home -> {
-                    replaceFragment(HomeFragment())
+                    if (!hasUsageStatsPermission()) {
+                        // Show a message to the user to grant the permission
+                        // You can use a Dialog, Snackbar, Toast, etc.
+                    } else {
+                        replaceFragment(HomeFragment())
+                    }
                     true
                 }
                 R.id.bottom_list -> {
@@ -31,8 +51,20 @@ class MainActivity2 : AppCompatActivity() {
                 else -> false
             }
         }
-        replaceFragment(HomeFragment())
+        if (hasUsageStatsPermission()) {
+            replaceFragment(HomeFragment())
+        } else {
+            // Show a message to the user to grant the permission
+            // You can use a Dialog, Snackbar, Toast, etc.
+        }
     }
+
+    private fun hasUsageStatsPermission(): Boolean {
+        val appOpsManager = getSystemService(Context.APP_OPS_SERVICE) as AppOpsManager
+        val mode = appOpsManager.checkOpNoThrow(AppOpsManager.OPSTR_GET_USAGE_STATS, android.os.Process.myUid(), packageName)
+        return mode == AppOpsManager.MODE_ALLOWED
+    }
+
     private fun replaceFragment(fragment: Fragment){
         supportFragmentManager.beginTransaction().replace(R.id.frame_container,fragment).commit()
     }
